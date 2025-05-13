b9810f72cef5c7dbb1c035ee74ca8cfe7bd5c35f
Chien-2112 <tranchien2121@gmail.com>
2025-05-13 11:09:21 +0700

    adds a load more future

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import EventList from '@/components/EventList'
-import Hero from '@/components/Hero'
-import { getEvents } from '@/services/blockchain'
-import { EventStruct } from '@/utils/type.dt'
-import { NextPage } from 'next'
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import EventList from "@/components/EventList";
+import Hero from "@/components/Hero";
+import { getEvents } from "@/services/blockchain";
+import { EventStruct } from "@/utils/type.dt";
+import { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
 const Page: NextPage<{ eventsData: EventStruct[] }> = ({ eventsData }) => {
-  const [end, setEnd] = useState<number>(6)
-  const [count] = useState<number>(6)
-  const [collection, setCollection] = useState<EventStruct[]>([])
+  const [end, setEnd] = useState<number>(6);
+  const [count] = useState<number>(6);
+  const [collection, setCollection] = useState<EventStruct[]>([]);
 
   useEffect(() => {
     setCollection(eventsData.slice(0, end))
@@ -18,20 +18,19 @@ const Page: NextPage<{ eventsData: EventStruct[] }> = ({ eventsData }) => {
   return (
     <div>
       <Head>
-        <title>Event X</title>
+        <title>Event C</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Hero />
       <EventList events={collection} />
 
-      <div className="mt-10 h-20 "></div>
+      <div className='mt-10 h-20'></div>
 
       {collection.length > 0 && eventsData.length > collection.length && (
-        <div className="w-full flex justify-center items-center">
+        <div className='w-full flex justify-center items-center'>
           <button
-            className="bg-orange-500 shadow-md rounded-full py-3 px-4
-        text-white duration-300 transition-all"
+            className="bg-orange-500 shadow-md rounded-full py-3 px-4 text-while duration-300 transition-all"
             onClick={() => setEnd(end + count)}
           >
             {' '}
@@ -45,8 +44,8 @@ const Page: NextPage<{ eventsData: EventStruct[] }> = ({ eventsData }) => {
 
 export default Page
 
-export const getServerSideProps = async () => {
-  const eventsData: EventStruct[] = await getEvents()
+export const getServerSideProps = async() => {
+  const eventsData: EventStruct[] = await getEvents();
   return {
     props: { eventsData: JSON.parse(JSON.stringify(eventsData)) },
   }
